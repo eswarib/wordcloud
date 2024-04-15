26f5a41bba4e601aefe615e7e57257492df4ecf0
Eswari M <shankareswary@gmail.com>
2024-04-15 09:50:40 +0100

    WordCloud First Commit.

---
+++ popup/popup.js
@@ -0,0 +1,74 @@
+
+async function getCurrentTabContent() {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    const contentPromise = await chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: () => {
+        const textContent = document.documentElement.innerText;
+        const url = window.location.href;
+        console.log("getCurrentTabContent::contentscript - " + textContent.substring(0,20));
+        console.log("getCurrentTabContent::url - " + url);
+    
+        return { textContent, url };
+      },
+    });
+    return contentPromise[0].result;
+  }
+  
+// Function to handle receiving word cloud image data from the server
+function handleImageData(imageData) {
+    console.log("imageData is");
+    console.log(imageData)
+
+    // Extract base64-encoded image data from the JSON response
+    //var base64ImageData = imageData['wordcloud'];
+
+    // Create an image element
+    var img = document.createElement('img');
+
+    // Set the source of the image to the base64-encoded image data
+    img.src = 'data:image/png;base64,' + imageData;
+
+    // Append the image to the popup
+    const wordCloudDiv = document.getElementById('wordCloud');
+    wordCloudDiv.innerHTML = '';
+    wordCloudDiv.appendChild(img);
+}
+
+
+// Function to handle receiving word cloud data from content script
+function handleWordCloudData(data) {
+    console.log('data to form wordcloud:', data.textContent);
+    // Fetch image data from Flask server
+    fetch('http://127.0.0.1:5000/generate_wordcloud', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    //    body: JSON.stringify({ text: data }) // Send the word cloud data received from content script
+    //    body: JSON.stringify({
+    //        text: data.textContent,
+    //    }),
+          body: JSON.stringify({ text: data.textContent }) // Send the word cloud data received from content script
+    })
+    .then(response => response.text())
+    .then(imageData => {
+        handleImageData(imageData);
+    })
+    .catch(error => {
+        console.error('Error fetching image:', error);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const content = await getCurrentTabContent();
+    await handleWordCloudData(content);
+
+    const popupWindow = window.chrome.extension.getViews({ type: 'popup' })[0];
+    const body = popupWindow.document.body;
+    const height = Math.min(body.scrollHeight + 40, 600); // Add 40px for padding
+    chrome.windows.update(popupWindow.outerWindowId, { width, height });
+  
+  });
+
+console.log("I am inside the popup.js");
---
+++ server/server.py
@@ -0,0 +1,43 @@
+# server.py
+
+from flask import Flask, request, send_file
+from flask_cors import CORS
+from wordcloud import WordCloud
+import base64
+import io
+
+from PIL import Image, ImageFont, ImageDraw
+# Create a new image with a white background
+image = Image.new("RGB", (500, 500), "white")
+
+font = ImageFont.truetype("/home/eswari/Documents/Personal/chromeExtensionProject/wordcloud/fonts/HappySwirly-KVB7l.ttf", size=12)
+draw = ImageDraw.Draw(image)
+text_size = draw.textlength("Your text here", font=font)
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/generate_wordcloud', methods=['POST'])
+def generate_wordcloud():
+    print(request.json)
+    text = request.json['text']
+
+  # Specify the path to a TrueType font file
+    font_path = "/home/eswari/Documents/Personal/chromeExtensionProject/wordcloud/fonts/Helvetica.ttf"
+    
+    # Create WordCloud with TrueType font specified
+    wordcloud = WordCloud(font_path=font_path).generate(text)
+    
+    # Convert the word cloud image to a base64-encoded string
+    img_data = io.BytesIO()
+    wordcloud.to_image().save(img_data, format='PNG')
+    img_data.seek(0)
+    img_base64 = base64.b64encode(img_data.read()).decode()
+
+    #return {'wordcloud': img_base64}
+    return img_base64
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
