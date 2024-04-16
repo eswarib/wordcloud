17a803a18411b368c6e9792b2e7a8c92dc51a73f
Eswari M <shankareswary@gmail.com>
2024-04-16 11:54:05 +0100

    port on which server will listen is made dynamic for heroku

--- server/server.py
+++ server/server.py
@@ -38,6 +38,13 @@ def generate_wordcloud():
     return img_base64
     
 
+//if __name__ == '__main__':
+//    app.run(debug=True)
+
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
